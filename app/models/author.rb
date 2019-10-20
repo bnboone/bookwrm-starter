cc064b98d2a99ca3c1f1043d9a940fc22c2662a8
bnboone <bnboone@cps.edu>
2019-10-20 11:07:51 -0500

    add book model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+	validates_presence_of :first_name, :last_name
+	scope :alphabetical, -> { order(last_name: :asc) }
+	has_many :books 
+	def full_name 
+		"#{first_name} #{last_name}"
+	end
 end
